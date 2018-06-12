8ed30059153dbb01e3a9919c3aa5f6c330f66d32
williamsg <williamsg@163.com>
2018-06-12 20:54:48 +0800

    Add xmax net plugin skeleton

---
+++ plugins/xmaxnet_plugin/xmaxnet_plugin.cpp
@@ -0,0 +1,27 @@
+#include <xmaxnet_plugin.hpp>
+
+
+
+//--------------------------------------------------
+Xmax::xmaxnet_plugin::xmaxnet_plugin()
+{
+
+}
+
+//--------------------------------------------------
+Xmax::xmaxnet_plugin::~xmaxnet_plugin()
+{
+
+}
+
+//--------------------------------------------------
+void Xmax::xmaxnet_plugin::initialize(const variables_map& options)
+{
+
+}
+
+//--------------------------------------------------
+void Xmax::xmaxnet_plugin::startup()
+{
+
+}
