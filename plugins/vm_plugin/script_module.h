70f067ab34a3da3192942eeb67aa443bd492f0de
赵航 <hangzhao@126.com>
2018-06-13 15:18:19 +0800

    Update script module interface.

--- plugins/vm_plugin/script_module.h
+++ plugins/vm_plugin/script_module.h
@@ -8,30 +8,29 @@ namespace Xmax {
 	namespace ScriptV8 {
 
 		using namespace v8;
+
 		class ScriptMoudle {
+
 		public:
+
 			ScriptMoudle();
 			virtual ~ScriptMoudle();
 
 			void Init();
-			void LoadCode(const std::string& code);
-			void Call(const std::string& fooName);
-			void UnloadCode();
+			//void LoadCode(const std::string& code);
+			void Call(const std::string& code, const std::string& fooName);
+			//void UnloadCode();
 			void Discard();
 
 		private:
 
-			void ScriptMoudle::vm_init(const HandleScope& scope, const Local<ObjectTemplate>& global, const Local<Context>& context, const Context::Scope& ctxScope);
+			void DoworkInContext(const HandleScope& scope, const Local<ObjectTemplate>& global, const Local<Context>& context, const Context::Scope& ctxScope);
 
-			std::string m_CurrentCode;
+			std::string				 m_CurrentCode;
+			std::string				 m_MainFooName;
 
-			Local<Script>			 m_Script;
 			Isolate*                 m_pIsolate;
-			Local<Context>           m_Context;
 
-			Isolate::Scope*          m_pIsolateScope;
-			HandleScope*             m_pHandleScope;
-			Context::Scope*			 m_pContextScope;
 		};
 	}
 }
