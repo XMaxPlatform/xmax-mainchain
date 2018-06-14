0dfd46d3ef9be4cce9522de31adaae943ff12fdb
williamsg <williamsg@163.com>
2018-06-14 18:14:08 +0800

    Change convention to keep consistent

--- foundation/application/include/pluginface.hpp
+++ foundation/application/include/pluginface.hpp
@@ -30,7 +30,7 @@ namespace xmaxapp
 
 		const string& GetName() const
 		{
-			return plugin_name;
+			return plugin_name_;
 		}
 
 		void InitOptions(OptionsDesc& cli, OptionsDesc& cfg)
@@ -43,13 +43,13 @@ namespace xmaxapp
 
 	private:
 		PluginFactory(const string& _name, const std::function<PluginFactoryFunction>& _function, const std::function<PluginInitOptions>& _function2)
-			: plugin_name(_name)
+			: plugin_name_(_name)
 			, create_function_(_function)
 			, init_options_(_function2)
 		{
 
 		}
-		string plugin_name;
+		string plugin_name_;
 		std::function<PluginFactoryFunction> create_function_;
 		std::function<PluginInitOptions> init_options_;
 
@@ -71,48 +71,48 @@ namespace xmaxapp
 
 		virtual State GetState() const override
 		{
-			return plugin_state;
+			return plugin_state_;
 		}		
 		virtual ApplicationBase* GetApp() const override
 		{
-			return _plugin_owner;
+			return plugin_owner_;
 		}
 
 		virtual void Initialize(const VarsMap& options)
 		{
-			plugin_state = Plugin::State::initialized;
+			plugin_state_ = Plugin::State::initialized;
 		}
 
 		virtual void Startup()
 		{
-			plugin_state = Plugin::State::startuped;
+			plugin_state_ = Plugin::State::startuped;
 		}
 		virtual void Shutdown()
 		{
-			plugin_state = Plugin::State::stopped;
+			plugin_state_ = Plugin::State::stopped;
 		}
 	protected:
 		PluginFace()
-			: plugin_state(Plugin::State::unknown)
-			, _plugin_owner(nullptr)
+			: plugin_state_(Plugin::State::unknown)
+			, plugin_owner_(nullptr)
 		{
 
 		}
 		template<typename TPlugin>
-		static TPlugin* create_plugin_impl(ApplicationBase* _owner)
+		static TPlugin* CreatePluginImpl(ApplicationBase* _owner)
 		{
 			TPlugin* _self = new TPlugin();
-			_self->set_owner(_owner);
+			_self->SetOwner(_owner);
 			return _self;
 		}
 
 	private:
-		void set_owner(ApplicationBase* _owner)
+		void SetOwner(ApplicationBase* _owner)
 		{
-			_plugin_owner = _owner;
+			plugin_owner_ = _owner;
 		}
-		Plugin::State plugin_state;
-		ApplicationBase* _plugin_owner;
+		Plugin::State plugin_state_;
+		ApplicationBase* plugin_owner_;
 	};
 
 }
@@ -136,7 +136,7 @@ namespace xmaxapp
 	private:\
 		static xmaxapp::PluginFace* createPlugin(xmaxapp::ApplicationBase* _owner)\
 		{\
-			return create_plugin_impl<plugin_self>(_owner);\
+			return CreatePluginImpl<plugin_self>(_owner);\
 		}
 
 //#define ALWAYS_REGIST_PLUGIN(plugin_self) \
