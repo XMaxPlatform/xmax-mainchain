20c3811da86f29a4daff4e8d1b68abb34d83f490
wenyang <419962937@qq.com>
2018-06-15 18:45:25 +0800

    add BOOST_TEST_MODULE

--- test/script_test/script_test.cpp
+++ test/script_test/script_test.cpp
@@ -1,4 +1,21 @@
-int main()
+#define BOOST_TEST_MODULE module_test
+
+#include <blockchain_plugin.hpp>
+#include <blockbuilder_plugin.hpp>
+#include<boost/test/included/unit_test.hpp>
+
+using namespace xmaxapp;
+using namespace xmax;
+using namespace boost::unit_test;
+
+BOOST_AUTO_TEST_SUITE (blockchain_plugin_test)
+
+BOOST_AUTO_TEST_CASE(bpt_1)
 {
-	return 0;
+	BlockChainPlugin::RegistSelf();
+	bool bret = BlockChainPlugin::RegistSelf();
+	BOOST_CHECK(bret == false);
 }
+
+BOOST_AUTO_TEST_SUITE_END()
+
