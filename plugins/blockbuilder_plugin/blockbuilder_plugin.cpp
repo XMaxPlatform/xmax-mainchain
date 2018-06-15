bdf329ff9287f8fea1a393cf7103a2215c66de88
fengzi <liuxingzdh@163.com>
2018-06-15 17:28:24 +0800

    block builder intial logic.

--- plugins/blockbuilder_plugin/blockbuilder_plugin.cpp
+++ plugins/blockbuilder_plugin/blockbuilder_plugin.cpp
@@ -3,10 +3,35 @@
 *  @copyright defined in xmax/LICENSE
 */
 #include <pro/log/log.hpp>
+#include <boost/asio/io_service.hpp>
 #include "blockbuilder_plugin.hpp"
 
 namespace xmax
 {
+	class BlockBuilderImpl
+	{
+	public:
+		boost::asio::deadline_timer timer;
+
+
+		BlockBuilderImpl(boost::asio::io_service& io)
+			: timer(io)
+		{
+
+		}
+	};
+
+	BlockBuilderPlugin::BlockBuilderPlugin()
+	{
+
+	}
+
+	void BlockBuilderPlugin::Initialize(const VarsMap& options)
+	{
+		Super::Initialize(options);
+
+		impl_ = std::make_unique<BlockBuilderImpl>(*GetApp()->GetService());
+	}
 
 	void BlockBuilderPlugin::Startup()
 	{
--- plugins/blockbuilder_plugin/include/blockbuilder_plugin.hpp
+++ plugins/blockbuilder_plugin/include/blockbuilder_plugin.hpp
@@ -9,15 +9,22 @@
 namespace xmax
 {
 	using xmaxapp::OptionsDesc;
+	using xmaxapp::VarsMap;
+
+	class BlockBuilderImpl;
 	class BlockBuilderPlugin : public xmaxapp::PluginFace
 	{
 		GENERATED_PLUGIN(BlockBuilderPlugin, xmaxapp::PluginFace, &InitOptions)
 	public:
+		BlockBuilderPlugin();
 
+		virtual void Initialize(const VarsMap& options) override;
 		virtual void Startup() override;
 
 	protected:
 		static void InitOptions(OptionsDesc& cli, OptionsDesc& cfg);
 
+
+		std::unique_ptr<BlockBuilderImpl> impl_;
 	};
 }
