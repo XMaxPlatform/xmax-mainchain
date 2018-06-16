c25fb50c9ba17a4385f05094fb6d360e2ede2611
williamsg <williamsg@163.com>
2018-06-16 20:26:47 +0800

    Move the network test project and link it to the protobuf lib

--- network/test/network_test.cpp
+++ network/test/network_test.cpp
@@ -4,9 +4,9 @@
 #include <boost/test/included/unit_test.hpp>
 
 
-BOOST_AUTO_TEST_SUITE(network_test_suite01)
+BOOST_AUTO_TEST_SUITE(network_protobuf)
 
-BOOST_AUTO_TEST_CASE(net_01)
+BOOST_AUTO_TEST_CASE(simple_proto_case)
 {
 	bool bret = true;
 	BOOST_CHECK(bret == false);
