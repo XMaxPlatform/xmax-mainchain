c4eb14cadf89449e3ebb52918ea4f301bbf5bbc5
williamsg <williamsg@163.com>
2018-06-17 14:01:23 +0800

    Add proto test

--- network/test/network_test.cpp
+++ network/test/network_test.cpp
@@ -2,14 +2,18 @@
 
 
 #include <boost/test/included/unit_test.hpp>
+#include <test.pb.h>
 
 
 BOOST_AUTO_TEST_SUITE(network_protobuf)
 
-BOOST_AUTO_TEST_CASE(simple_proto_case)
+BOOST_AUTO_TEST_CASE(proto_set_int32_field)
 {
-	bool bret = true;
-	BOOST_CHECK(bret == false);
+	using namespace tutorial;
+	Person person_msg;
+	person_msg.set_id(12345);
+	BOOST_CHECK(person_msg.id() == 12345);
+
 }
 
 
