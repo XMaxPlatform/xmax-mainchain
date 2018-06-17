404aaf20b498b0ff57e86c0d3c71dd9b2476e94a
williamsg <williamsg@163.com>
2018-06-17 15:04:05 +0800

    Add shut down logic to the protobuf to solve the memory leak problem

--- network/test/network_test.cpp
+++ network/test/network_test.cpp
@@ -2,6 +2,7 @@
 
 
 #include <boost/test/included/unit_test.hpp>
+#include <google/protobuf/stubs/common.h>
 #include <test.pb.h>
 
 
@@ -18,3 +19,23 @@ BOOST_AUTO_TEST_CASE(proto_set_int32_field)
 
 
 BOOST_AUTO_TEST_SUITE_END()
+
+
+
+
+namespace {
+
+	using namespace google::protobuf;
+	// Force shutdown at process exit so that we can test for memory leaks.  To
+	// actually check for leaks, I suggest using the heap checker included with
+	// google-perftools.  Set it to "draconian" mode to ensure that every last
+	// call to malloc() has a corresponding free().
+	struct ForceShutdown {
+		~ForceShutdown() {
+			ShutdownProtobufLibrary();
+			// Test to shutdown the library twice, which should succeed.
+			ShutdownProtobufLibrary();
+		}
+	} force_shutdown;
+
+}  // namespace
