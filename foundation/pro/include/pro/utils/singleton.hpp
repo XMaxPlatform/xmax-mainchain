bd430b1a09e70ffeb8e71e390498afbb77f85c9b
SAIKYOU1986 <419962937@qq.com>
2018-06-17 16:21:57 +0800

    fix singleton bug

--- foundation/pro/include/pro/utils/singleton.hpp
+++ foundation/pro/include/pro/utils/singleton.hpp
@@ -3,9 +3,15 @@
 namespace pro
 {
 	template <class T>
-	class Singleton {	
+	class Singleton {
+	private:
 		Singleton<T>(const Singleton<T>&) = delete;
 		Singleton<T>& operator=(const Singleton<T>&) = delete;
+
+	void* operator new(size_t nSize)
+	{
+		return malloc(nSize);
+	}
 
 	protected:
 		Singleton<T>() = default;
