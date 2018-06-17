8f5d1f398f890122880d87a02d6a32e7a6165ae4
williamsg <williamsg@163.com>
2018-06-17 16:33:27 +0800

    Add proto test

--- network/test/network_test.cpp
+++ network/test/network_test.cpp
@@ -5,16 +5,24 @@
 #include <google/protobuf/stubs/common.h>
 #include <test.pb.h>
 
+using namespace tutorial;
 
 BOOST_AUTO_TEST_SUITE(network_protobuf)
 
 BOOST_AUTO_TEST_CASE(proto_set_int32_field)
 {
-	using namespace tutorial;
+	
 	Person person_msg;
 	person_msg.set_id(12345);
 	BOOST_CHECK(person_msg.id() == 12345);
 
+}
+
+BOOST_AUTO_TEST_CASE(proto_set_string_field)
+{
+	Person person_msg;
+	person_msg.set_email("12345");	
+	BOOST_CHECK(person_msg.email() == "12345");
 }
 
 
