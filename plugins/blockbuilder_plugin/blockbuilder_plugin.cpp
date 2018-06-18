974132657e42b35ee91c2ff35d6e0234dfdaf270
fz <liuxingzdh@gmail.com>
2018-06-18 23:48:22 +0800

    comment of BlockBuilderPlugin

--- plugins/blockbuilder_plugin/blockbuilder_plugin.cpp
+++ plugins/blockbuilder_plugin/blockbuilder_plugin.cpp
@@ -21,7 +21,6 @@ namespace xmax
 		{
 
 		}
-
 		void StartLoop()
 		{
 
--- plugins/blockbuilder_plugin/include/blockbuilder_plugin.hpp
+++ plugins/blockbuilder_plugin/include/blockbuilder_plugin.hpp
@@ -12,15 +12,21 @@ namespace xmax
 	using xmaxapp::VarsMap;
 
 	class BlockBuilderImpl;
+
+
+/*!
+ * \class BlockBuilderPlugin
+ * \brief The builder plugin use for build logic.
+ */
 	class BlockBuilderPlugin : public xmaxapp::PluginFace
 	{
 		GENERATED_PLUGIN(BlockBuilderPlugin, xmaxapp::PluginFace, &InitOptions)
 	public:
 		BlockBuilderPlugin();
-
+		// xmaxapp::PluginFace interface ----------------------------------
 		virtual void Initialize(const VarsMap& options) override;
 		virtual void Startup() override;
-
+		// ----------------------------------------------------------------
 	protected:
 
 		virtual void OnCreated() override;
