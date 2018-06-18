270bfcf826f35f3b93a255267027f345d3b719a2
SAIKYOU1986 <419962937@qq.com>
2018-06-18 22:44:41 +0800

    add comment

--- foundation/pro/include/pro/types/generictypes.hpp
+++ foundation/pro/include/pro/types/generictypes.hpp
@@ -7,15 +7,40 @@
 #include <string>
 namespace pro
 {
+	/**
+	* typedef of string, using stl version
+	*/
 	using string = std::string;
-
+	/**
+	* typedef of uint8, using stl version
+	*/
 	using uint8 = std::uint8_t;
+	/**
+	* typedef of uint16, using stl version
+	*/
 	using uint16 = std::uint16_t;
+	/**
+	* typedef of uint32, using stl version
+	*/
 	using uint32 = std::uint32_t;
+	/**
+	* typedef of uint64, using stl version
+	*/
 	using uint64 = std::uint64_t;
-
+	/**
+	* typedef of int8, using stl version
+	*/
 	using int8 = std::int8_t;
+	/**
+	* typedef of int16, using stl version
+	*/
 	using int16 = std::int16_t;
+	/**
+	* typedef of int32, using stl version
+	*/
 	using int32 = std::int32_t;
+	/**
+	* typedef of int64, using stl version
+	*/
 	using int64 = std::int64_t;
 }
