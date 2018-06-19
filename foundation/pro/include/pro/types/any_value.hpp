947e3cb1884a1cca0f70a62cdb4a34dbdf496d68
fengzi <liuxingzdh@163.com>
2018-06-19 21:32:26 +0800

    lib any base.

--- foundation/pro/include/pro/types/any_value.hpp
+++ foundation/pro/include/pro/types/any_value.hpp
@@ -5,15 +5,54 @@
 #pragma once
 #include <pro/types/build.hpp>
 
+#include <memory>
+#include <vector>
+#include <pro/types/generictypes.hpp>
+
 namespace pro
 {
+
+	class AnyEntity
+	{
+	public:
+		enum TypeCode
+		{
+			UI64,
+			I64,
+			String,
+		};
+		AnyEntity() = default;
+		AnyEntity(const AnyEntity&) = delete;
+		AnyEntity& operator = (const AnyEntity &) = delete;
+
+		union Entity
+		{
+
+		};
+	};
 
 	class AnyValue
 	{
 	public:
+		AnyValue() = default;
+		AnyValue(const AnyValue&) = delete;
+		AnyValue& operator = (const AnyValue &) = delete;
+		~AnyValue();
+		typedef string EntityKey;
+		struct EntityPair
+		{
+			EntityKey key;
+			AnyEntity entity;
+		};
+		typedef std::vector<EntityPair> EntityContainer;
+		typedef EntityContainer::iterator Iterator;
+		typedef EntityContainer::const_iterator ConstIterator;
 
+		void Clear();
 	private:
 
+
+		EntityContainer entities_;
 	};
 
 }
---
+++ foundation/pro/types/any_value.cpp
@@ -0,0 +1,19 @@
+/**
+*  @file
+*  @copyright defined in xmax/LICENSE
+*/
+#include <pro/types/any_value.hpp>
+
+namespace pro
+{
+	AnyValue::~AnyValue()
+	{
+
+	}
+
+	void AnyValue::Clear()
+	{
+
+	}
+
+}
