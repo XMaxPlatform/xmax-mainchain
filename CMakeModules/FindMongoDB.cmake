bab649f4904e9368cb7537b71e8add5d9bf083c3
williamsg <williamsg@163.com>
2018-06-20 16:49:04 +0800

    Add find mongo db cmake module config file. Add the initial description

---
+++ CMakeModules/FindMongoDB.cmake
@@ -0,0 +1,20 @@
+# Find mongo db libraries and directories
+#
+# Usage
+#
+#     find_package(MongoDB)
+#
+# In order to find the libraries, you need to pass these variables:
+#
+#   MONGO_DB_C_ROOT: Set this variable to the ROOT dir of the mongo-c-driver
+#
+#   MONGO_DB_CXX_ROOT: Set this variable to the ROOT dir of the mongo-cxx-driver
+#
+#
+# Variables output:
+#
+#  MongoDB_FOUND                 If found all the prerequisites
+#  MongoDB_LIBRARIES             Libraries of the mongo db
+#  MongoDB_INCLUDE_DIR           Include directories of the mongo db
+
+
