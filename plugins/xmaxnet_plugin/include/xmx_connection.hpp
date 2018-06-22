3f01b6f1a2cfacc2c13c4adcfa13fae34d87ee92
wenyang <419962937@qq.com>
2018-06-22 17:48:22 +0800

    add XMX_Connection

--- plugins/xmaxnet_plugin/include/xmx_connection.hpp
+++ plugins/xmaxnet_plugin/include/xmx_connection.hpp
@@ -1,9 +1,27 @@
 #pragma once
-#include <memory>
 
-class XMX_Connection : public std::enable_shared_from_this<XMX_Connection>
+#include <memory>
+#include <string>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/ip/host_name.hpp>
+
+namespace xmax
+{
+	using boost::asio::ip::tcp;
+	using boost::asio::ip::address_v4;
+	using boost::asio::ip::host_name;
+
+
+	class XMX_Connection : public std::enable_shared_from_this<XMX_Connection>
 {
 public:
+
+	 XMX_Connection(const std::string& endpoint);
+
+	 XMX_Connection(const std::shared_ptr<tcp::socket>& s);
+
 protected:
 private:
 };
+
+}
--- plugins/xmaxnet_plugin/xmx_connection.cpp
+++ plugins/xmaxnet_plugin/xmx_connection.cpp
@@ -1,1 +1,15 @@
 #include "xmx_connection.hpp"
+
+namespace xmax
+{
+XMX_Connection::XMX_Connection(const std::string& endpoint)
+{
+
+}
+
+XMX_Connection::XMX_Connection(const std::shared_ptr<tcp::socket>& s)
+{
+
+}
+
+}
