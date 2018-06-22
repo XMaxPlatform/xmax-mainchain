8a555f7891baaab3be77bc02222c9fe87255797e
fz <liuxingzdh@gmail.com>
2018-06-23 00:39:23 +0800

    move pro test code.

--- test/pro_test/pro_test.cpp
+++ test/pro_test/pro_test.cpp
@@ -2,6 +2,10 @@
 
 #include <boost/test/included/unit_test.hpp>
 #include <pro/utils/singleton.hpp>
+#include <pro/types/time.hpp>
+#include <pro/types/any_value.hpp>
+
+using namespace pro;
 
 BOOST_AUTO_TEST_SUITE(pro_utils_suite)
 
@@ -30,6 +34,43 @@ BOOST_AUTO_TEST_CASE(test_singleton) {
 	BOOST_CHECK(s1.GetA() == 12345);
 }
 
+BOOST_AUTO_TEST_SUITE(pro_time_test)
+
+BOOST_AUTO_TEST_CASE(pro_time_1)
+{
+	TimeMilliseconds milli(2500ll);
+	TimeMicroseconds micro = milli.ToTime<TimeMicroseconds>();
+	TimeMilliseconds milli2 = micro.ToTime<TimeMilliseconds>();
+
+	TimeSeconds sec = micro.ToTime<TimeSeconds>();
+
+	TimeMicroseconds micro2 = sec.ToTime<TimeMicroseconds>();
+	TimeMilliseconds milli3 = sec.ToTime<TimeMilliseconds>();
+
+	BOOST_CHECK(milli.GetValue() == 2500ll);
+	BOOST_CHECK(micro.GetValue() == 2500ll * 1000ll);
+	BOOST_CHECK(milli2.GetValue() == 2500ll);
+
+	BOOST_CHECK(sec.GetValue() == 2ll);
+	BOOST_CHECK(micro2.GetValue() == 2ll * 1000ll * 1000ll);
+	BOOST_CHECK(milli3.GetValue() == 2ll * 1000ll);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
+BOOST_AUTO_TEST_SUITE(any_value)
+
+BOOST_AUTO_TEST_CASE(any_value)
+{
+	pro::AnyVaule anyi32 = (int32_t)53;
+	pro::AnyVaule anyi64 = (int64_t)64;
+	pro::AnyVaule anystr = "test string";
+
+
+}
+
+BOOST_AUTO_TEST_SUITE_END()
 
 
 BOOST_AUTO_TEST_SUITE_END()
--- test/script_test/script_test.cpp
+++ test/script_test/script_test.cpp
@@ -3,63 +3,19 @@
 #include <blockchain_plugin.hpp>
 #include <blockbuilder_plugin.hpp>
 #include "script_module.hpp"
-#include <pro/types/time.hpp>
-#include <pro/types/any_value.hpp>
+
 #include <boost/test/included/unit_test.hpp>
 #include "pro/utils/singleton.hpp"
 
 using namespace xmaxapp;
 using namespace xmax;
 using namespace boost::unit_test;
-using namespace pro;
+
 
 //#define Test_Script
 
 class SingletonImpl : public Singleton<SingletonImpl>
 {};
-
-BOOST_AUTO_TEST_SUITE(time_test)
-
-BOOST_AUTO_TEST_CASE(time_1)
-{
-	TimeMilliseconds milli(2500ll);
-	TimeMicroseconds micro = milli.ToTime<TimeMicroseconds>();
-	TimeMilliseconds milli2 = micro.ToTime<TimeMilliseconds>();
-
-	TimeSeconds sec = micro.ToTime<TimeSeconds>();
-
-	TimeMicroseconds micro2 = sec.ToTime<TimeMicroseconds>();
-	TimeMilliseconds milli3 = sec.ToTime<TimeMilliseconds>();
-
-	BOOST_CHECK(milli.GetValue() == 2500ll);
-	BOOST_CHECK(micro.GetValue() == 2500ll * 1000ll);
-	BOOST_CHECK(milli2.GetValue() == 2500ll);
-
-	BOOST_CHECK(sec.GetValue() == 2ll);
-	BOOST_CHECK(micro2.GetValue() == 2ll * 1000ll * 1000ll);
-	BOOST_CHECK(milli3.GetValue() == 2ll * 1000ll);
-}
-
-BOOST_AUTO_TEST_SUITE_END()
-
-
-BOOST_AUTO_TEST_SUITE(any_value)
-
-BOOST_AUTO_TEST_CASE(any_value)
-{
-	pro::AnyVaule anyi32;
-	pro::AnyVaule anyi64;
-	pro::AnyVaule anystr;
-
-	anyi32.SetValue((int32_t)53);
-	anyi64.SetValue((int64_t)64);
-
-	anystr.SetValue("xxxxxf");
-
-
-}
-
-BOOST_AUTO_TEST_SUITE_END()
 
 
 BOOST_AUTO_TEST_SUITE (blockchain_plugin_test)
