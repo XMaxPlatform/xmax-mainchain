21930771301983e07a182bb88f747d946997dc6e
fengzi <liuxingzdh@163.com>
2018-06-30 15:40:33 +0800

    any object base.

--- foundation/pro/include/pro/types/any_object.hpp
+++ foundation/pro/include/pro/types/any_object.hpp
@@ -14,22 +14,83 @@ namespace pro
 	{
 	public:
 		AnyObject() = default;
-		AnyObject(const AnyObject&) = delete;
-		AnyObject& operator = (const AnyObject &) = delete;
+
 		~AnyObject();
 		typedef string EntityKey;
 		struct Entity
 		{
 			EntityKey key;
 			AnyValue entity;
+			Entity() = default;
+			Entity(const EntityKey& k)
+				: key(k)
+			{
+			}
+			Entity(EntityKey&& k)
+				: key(std::forward<EntityKey>(k))
+			{
+			}
+
 		};
 		typedef std::vector<AnyObject::Entity> EntityContainer;
 		typedef EntityContainer::iterator Iterator;
 		typedef EntityContainer::const_iterator ConstIterator;
 
+		
+		const AnyValue& At(const string& key) const;
+
+		AnyValue& At(const string& key);
+
+		void Set(string&& key, AnyValue&& val);
+
 		void Clear();
+
+
+		const AnyValue& operator [] (const string& key) const
+		{
+			return At(key);
+		}
+		AnyValue& operator [] (const string& key)
+		{
+			return At(key);
+		}
+
+
 	private:
-		EntityContainer entities_;
+
+		Iterator find(const string& key)
+		{
+			for (auto it = entities_.begin(); it != entities_.end(); ++it)
+			{
+				if (it->key == key)
+				{
+					return it;
+				}
+			}
+			return entities_.end();
+		}
+		ConstIterator find(const string& key) const
+		{
+			for (auto it = entities_.begin(); it != entities_.end(); ++it)
+			{
+				if (it->key == key)
+				{
+					return it;
+				}
+			}
+			return entities_.end();
+		}
+
+		AnyValue& emplaceback(string&& key) const
+		{
+			return entities_.emplace_back(Entity(key)).entity;
+		}
+		AnyValue& emplaceback(const string& key) const
+		{
+			return entities_.emplace_back(Entity(key)).entity;
+		}
+
+		mutable EntityContainer entities_;
 	};
 
 }
--- foundation/pro/types/any_object.cpp
+++ foundation/pro/types/any_object.cpp
@@ -6,6 +6,8 @@
 
 namespace pro
 {
+
+
 	AnyObject::~AnyObject()
 	{
 
@@ -13,7 +15,41 @@ namespace pro
 
 	void AnyObject::Clear()
 	{
+		entities_.clear();
+	}
 
+	const AnyValue& AnyObject::At(const string& key) const
+	{
+		auto it = find(key);
+		if (it != entities_.end())
+		{
+			return it->entity;
+		}
+		return emplaceback(key);
+	}
+
+	AnyValue& AnyObject::At(const string& key)
+	{
+		auto it = find(key);
+		if (it != entities_.end())
+		{
+			return it->entity;
+		}
+		return emplaceback(key);
+	}
+
+	void AnyObject::Set(string&& key, AnyValue&& val)
+	{
+		auto it = find(key);
+		if (it != entities_.end())
+		{
+			it->entity = val;
+		}
+		else
+		{
+			emplaceback(std::forward<string>(key)) = std::forward<AnyValue>(val);
+		}
+		
 	}
 
 }
