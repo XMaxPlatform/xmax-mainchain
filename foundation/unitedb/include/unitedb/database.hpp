2a492c168d13ca5b72685257a50be23d446c0bed
fengzi <liuxingzdh@163.com>
2018-07-05 21:34:14 +0800

    database.

--- foundation/unitedb/include/unitedb/database.hpp
+++ foundation/unitedb/include/unitedb/database.hpp
@@ -3,12 +3,23 @@
 *  @copyright defined in xmax/LICENSE
 */
 #pragma once
+
+#include <boost/interprocess/managed_mapped_file.hpp>
+#include <memory>
+
 namespace unitedb
 {
+	namespace inpr = boost::interprocess;
+	using managed_file = inpr::managed_mapped_file;
+
 	class Database
 	{
 	public:
 		Database();
 		~Database();
+
+
+	private:
+		std::unique_ptr<managed_file>                        managed_file_;
 	};
 }
