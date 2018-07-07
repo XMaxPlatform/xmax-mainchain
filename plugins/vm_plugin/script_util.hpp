410b1e16d24675e15e58355654568b7437019bd7
luow1226@sina.com <luow1226@sina.com>
2018-07-07 18:04:30 +0800

    change script logic

--- plugins/vm_plugin/script_util.hpp
+++ plugins/vm_plugin/script_util.hpp
@@ -9,12 +9,19 @@ namespace xmax {
 	namespace scriptv8{
 
 		using namespace v8;
+		using PersistentCpyableContext = v8::Persistent<v8::Context, v8::CopyablePersistentTraits<v8::Context>>;
 
 		typedef std::function<v8::Handle<v8::Value>(const HandleScope&, const Local<ObjectTemplate>&, const Local<Context>&, const Context::Scope&)> DoWorkInJsCtx;
 
 		typedef std::map<std::string, Local<FunctionTemplate>> JsFooBindMap;
 
-		v8::Handle<v8::Value> EnterJsContext(Isolate* pIsolate, DoWorkInJsCtx dowork);
+		void EnterJsContext(v8::Isolate* pIsolate, v8::Local<v8::ObjectTemplate>& global, DoWorkInJsCtx dowork);
+
+		PersistentCpyableContext CreateJsContext(v8::Isolate* pIsolate, v8::Local<v8::ObjectTemplate>& global);
+
+		void EnterJsContext(v8::Isolate* pIsolate, v8::Persistent<v8::Context, v8::CopyablePersistentTraits<v8::Context>>& context);
+
+		void ExitJsContext(v8::Isolate* pIsolate, v8::Persistent<v8::Context, v8::CopyablePersistentTraits<v8::Context>>& context);
 
 		void BindJsFoos(Isolate* pIsolate, const Local<ObjectTemplate>& fooGlobal, const JsFooBindMap& foosToBind);
 
