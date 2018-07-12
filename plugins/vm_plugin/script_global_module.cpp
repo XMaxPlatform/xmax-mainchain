dda820b0607cc8d778e03b881ab2b13ea0a768c0
hangzhao@126.com <hangzhao@126.com>
2018-07-12 19:47:39 +0800

    edit sciprt global

---
+++ plugins/vm_plugin/script_global_module.cpp
@@ -0,0 +1,16 @@
+#include "script_global_module.hpp"
+namespace xmax {
+
+	namespace scriptv8 {
+
+		ScriptGlobalMoudle::ScriptGlobalMoudle()
+		{
+
+		}
+
+		ScriptGlobalMoudle::~ScriptGlobalMoudle()
+		{
+
+		}
+	}
+}
