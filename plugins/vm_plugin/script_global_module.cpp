404ddd5c84a6e251e651c5e4e67b777d28eadb30
luow1226@sina.com <luow1226@sina.com>
2018-07-12 20:01:27 +0800

    imp script global

--- plugins/vm_plugin/script_global_module.cpp
+++ plugins/vm_plugin/script_global_module.cpp
@@ -1,4 +1,5 @@
 #include "script_global_module.hpp"
+#include <libplatform/libplatform.h>
 namespace xmax {
 
 	namespace scriptv8 {
@@ -15,7 +16,13 @@ namespace xmax {
 
 		void ScriptGlobalMoudle::SetupV8Env()
 		{
+			//V8_AddIntrinsicFoo("CallBackCheck", (void*)CallBackCheck, 2, 1);
 
+			V8::InitializeICUDefaultLocation("");
+			V8::InitializeExternalStartupData("");
+			Platform* platform = platform::CreateDefaultPlatform();
+			V8::InitializePlatform(platform);
+			V8::Initialize();
 		}
 
 		void ScriptGlobalMoudle::DiscardV8Env()
