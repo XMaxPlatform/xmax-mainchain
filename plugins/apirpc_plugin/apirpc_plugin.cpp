8b51f8d827a98fdc3114ab192dca38f00c36964c
williamsg <williamsg@163.com>
2018-08-03 16:49:32 +0800

    Add apirpc implementation class

--- plugins/apirpc_plugin/apirpc_plugin.cpp
+++ plugins/apirpc_plugin/apirpc_plugin.cpp
@@ -1,6 +1,39 @@
 #include "apirpc_plugin.hpp"
 
 namespace xmax {
+
+	/*!
+	 * \class ApiRpcPluginImpl
+	 *
+	 * \brief Implementation details of ApiRpcPlugin
+	 *
+	 */
+	class ApiRpcPluginImpl {
+		ApiRpcPluginImpl();
+		~ApiRpcPluginImpl();
+
+
+	};
+
+	//--------------------------------------------------
+	ApiRpcPluginImpl::ApiRpcPluginImpl()
+	{
+
+	}
+
+	//--------------------------------------------------
+	ApiRpcPluginImpl::~ApiRpcPluginImpl()
+	{
+
+	}
+
+
+	/*!
+	* \class ApiRpcPlugin
+	*
+	* \brief Implementation details of ApiRpcPlugin
+	*
+	*/
 	//--------------------------------------------------
 	void ApiRpcPlugin::InitOptions(OptionsDesc& cli, OptionsDesc& cfg)
 	{
@@ -10,7 +43,13 @@ namespace xmax {
 	//--------------------------------------------------
 	void ApiRpcPlugin::Initialize(const VarsMap& options)
 	{
+		impl_ = std::make_unique<ApiRpcPluginImpl>();
+	}
 
+	//--------------------------------------------------
+	void ApiRpcPlugin::Shutdown()
+	{
+		impl_.reset();
 	}
 
 	//--------------------------------------------------
--- plugins/apirpc_plugin/include/apirpc_plugin.hpp
+++ plugins/apirpc_plugin/include/apirpc_plugin.hpp
@@ -25,6 +25,7 @@ namespace xmax
 		// xmaxapp::PluginFace interface ----------------------------------
 		virtual void Initialize(const VarsMap& options) override;
 		virtual void Startup() override;
+		virtual void Shutdown() override;
 			
 		// ----------------------------------------------------------------
 	protected:
