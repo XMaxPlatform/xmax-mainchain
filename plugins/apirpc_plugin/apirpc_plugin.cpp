35bd2a3d47d9c764e38f85f6ea0c2062f6886819
williamsg <williamsg@163.com>
2018-08-03 16:28:36 +0800

    Add apirpc plugin framework

---
+++ plugins/apirpc_plugin/apirpc_plugin.cpp
@@ -0,0 +1,25 @@
+#include "apirpc_plugin.hpp"
+
+namespace xmax {
+	//--------------------------------------------------
+	void ApiRpcPlugin::InitOptions(OptionsDesc& cli, OptionsDesc& cfg)
+	{
+
+	}
+
+	//--------------------------------------------------
+	void ApiRpcPlugin::Initialize(const VarsMap& options)
+	{
+
+	}
+
+	//--------------------------------------------------
+	void ApiRpcPlugin::Startup()
+	{
+
+	}
+
+}
+
+
+
