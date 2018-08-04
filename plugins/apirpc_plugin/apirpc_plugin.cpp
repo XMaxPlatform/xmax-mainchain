013c40a04373155ed6eed35e17aaac37b61d8120
williamsg <williamsg@163.com>
2018-08-04 17:21:55 +0800

    Add allow cross origin option parsing

--- plugins/apirpc_plugin/apirpc_plugin.cpp
+++ plugins/apirpc_plugin/apirpc_plugin.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 namespace xmax {
 
+	static const char* const kHttpApiAddressOp = "http-api-address";
+	static const char* const kAllowCrossOriginOp = "api-allow-cross-origin";
+
 	/*!
 	 * \class ApiRpcPluginImpl
 	 *
@@ -17,6 +20,7 @@ namespace xmax {
 		~ApiRpcPluginImpl();
 
 
+		string allow_cross_origin;
 	};
 
 	//--------------------------------------------------
@@ -42,10 +46,10 @@ namespace xmax {
 	void ApiRpcPlugin::InitOptions(OptionsDesc& cli, OptionsDesc& cfg)
 	{
 		cfg.add_options()
-			("http-api-address", xpo::value<std::string>()->default_value("127.0.0.1:8081"), "default api rpc http address(ip:port)");
+			(kHttpApiAddressOp, xpo::value<std::string>()->default_value("127.0.0.1:8081"), "default api rpc http address(ip:port)");
 
 		cfg.add_options()
-			("api-allow-cross-origin", xpo::value<std::string>()->default_value("*"), "default api rpc http address(ip:port)");
+			(kAllowCrossOriginOp, xpo::value<std::string>()->default_value("*"), "default api rpc http address(ip:port)");
 
 
 	}
@@ -54,6 +58,10 @@ namespace xmax {
 	void ApiRpcPlugin::Initialize(const VarsMap& options)
 	{
 		impl_ = std::make_unique<ApiRpcPluginImpl>();
+
+		if (options.count(kAllowCrossOriginOp)) {
+			impl_->allow_cross_origin = options.at(kAllowCrossOriginOp).as<string>();
+		}
 	}
 
 	//--------------------------------------------------
