ec28e0a5aeea0eda6c1646de65343f4bbdd53135
hangzhao@126.com <hangzhao@126.com>
2018-08-07 10:21:35 +0800

    Rename u128 to i128

--- plugins/vm_plugin/script_interface/int128_bind.cpp
+++ plugins/vm_plugin/script_interface/int128_bind.cpp
@@ -11,24 +11,24 @@ namespace xmax {
 
 
 		//--------------------------------------------------
-		V8u128* V8u128::NewV8CppObj(const v8::FunctionCallbackInfo<v8::Value>& args)
+		V8i128* V8i128::NewV8CppObj(const v8::FunctionCallbackInfo<v8::Value>& args)
 		{
-			return new V8u128();
+			return new V8i128();
 		}
 
 
 		//--------------------------------------------------
-		void V8u128::ConstructV8Object(const v8::FunctionCallbackInfo<v8::Value>& args)
+		void V8i128::ConstructV8Object(const v8::FunctionCallbackInfo<v8::Value>& args)
 		{
 			if (!args.IsConstructCall())
 				return args.GetReturnValue().Set(Undefined(args.GetIsolate()));
 
-			V8u128* cpp_object = nullptr;
+			V8i128* cpp_object = nullptr;
 			if (args.Length() == 1)
 			{
 				Local<Object> self = args.Holder();
 				Local<External> wrap = Local<External>::Cast(args[0]);
-				cpp_object = (V8u128*)wrap->Value();
+				cpp_object = (V8i128*)wrap->Value();
 			}
 			else
 			{
@@ -45,16 +45,16 @@ namespace xmax {
 
 
 		//--------------------------------------------------
-		void V8u128::WeakExternalReferenceCallback(const WeakCallbackInfo<V8u128>& data)
+		void V8i128::WeakExternalReferenceCallback(const WeakCallbackInfo<V8i128>& data)
 		{
-			if (V8u128* cpp_object = data.GetParameter())
+			if (V8i128* cpp_object = data.GetParameter())
 			{
 				delete cpp_object;
 			}
 		}
 
 		//--------------------------------------------------
-		void V8u128::RegisterWithV8(v8::Isolate* isolate, v8::Handle<ObjectTemplate> global)
+		void V8i128::RegisterWithV8(v8::Isolate* isolate, v8::Handle<ObjectTemplate> global)
 		{
 			Handle<FunctionTemplate> func_template = FunctionTemplate::New(isolate, &ConstructV8Object);
 
