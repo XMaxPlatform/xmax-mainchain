1f3d23a226385d9154d53ac18f2ce87db96b0cbf
luow1226@sina.com <luow1226@sina.com>
2018-08-07 17:38:17 +0800

    bind with new method

--- plugins/vm_plugin/script_interface/uint128_bind.cpp
+++ plugins/vm_plugin/script_interface/uint128_bind.cpp
@@ -5,5 +5,25 @@ namespace xmax {
 	namespace scriptv8 {
 
 
+		V8u128* V8u128::NewV8CppObj(const v8::FunctionCallbackInfo<v8::Value>& args)
+		{
+			return nullptr;
+		}
+
+		void V8u128::RegisterWithV8(v8::Isolate* isolate, v8::Handle<v8::ObjectTemplate> global)
+		{
+
+		}
+
+		void V8u128::ConstructV8Object(const v8::FunctionCallbackInfo<v8::Value>& args)
+		{
+
+		}
+
+		void V8u128::WeakExternalReferenceCallback(const v8::WeakCallbackInfo<V8u128>& data)
+		{
+
+		}
+
 	}
 }
