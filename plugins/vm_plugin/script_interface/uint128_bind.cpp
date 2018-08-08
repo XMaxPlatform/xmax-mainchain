dcbbace1f46f8b1bbcdf40e76c3a4baccbf8ea88
luow1226@sina.com <luow1226@sina.com>
2018-08-08 10:31:32 +0800

    Script update

--- plugins/vm_plugin/script_interface/uint128_bind.cpp
+++ plugins/vm_plugin/script_interface/uint128_bind.cpp
@@ -16,6 +16,18 @@ namespace xmax {
 		{
 			Handle<FunctionTemplate> func_template = FunctionTemplate::New(isolate, &ConstructV8Object);
 
+			func_template->SetClassName(String::NewFromUtf8(
+				isolate,
+				TypeName(),
+				NewStringType::kNormal).ToLocalChecked());
+			func_template->InstanceTemplate()->SetInternalFieldCount(1);
+			global->Set(String::NewFromUtf8(
+				isolate,
+				TypeName(),
+				NewStringType::kNormal).ToLocalChecked(), func_template);
+
+
+			Handle<ObjectTemplate> proto_template = func_template->PrototypeTemplate();
 		}
 
 		void V8u128::ConstructV8Object(const v8::FunctionCallbackInfo<v8::Value>& args)
