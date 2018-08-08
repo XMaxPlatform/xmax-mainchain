f45facc3f701b9a7feed475e015af2a3f9f40500
wenyang <419962937@qq.com>
2018-08-08 15:52:12 +0800

    add new data struct

--- blockchain/chain/include/block.hpp
+++ blockchain/chain/include/block.hpp
@@ -1,1 +1,51 @@
 #pragma once
+#include <vector>
+#include <memory>
+
+namespace chain
+{
+	class block_header
+	{
+	public:
+	};
+
+	class signed_block_header : public block_header
+	{
+	public:
+	};
+
+	class transaction_receipt_header
+	{
+	public:
+	};
+
+	class transaction_receipt : public transaction_receipt_header
+	{
+	public:
+	};
+
+	class signed_block : public signed_block_header
+	{
+	public:
+		std::vector<transaction_receipt> receipts_;
+	};
+
+	class signed_block_list
+	{
+	public:
+		std::vector<signed_block>	blockList_;
+	};
+
+	using signed_block_ptr = std::shared_ptr<signed_block>;
+
+	class block_confirmation_header
+	{
+	public:
+
+	};
+
+	class block_confirmation : public block_confirmation_header
+	{
+
+	};
+}
