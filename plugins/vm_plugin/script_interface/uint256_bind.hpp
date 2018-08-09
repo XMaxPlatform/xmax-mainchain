377d070bd24781e5fc03abc6d5d5f2590455d943
luow1226@sina.com <luow1226@sina.com>
2018-08-09 18:27:08 +0800

    u256 add

--- plugins/vm_plugin/script_interface/uint256_bind.hpp
+++ plugins/vm_plugin/script_interface/uint256_bind.hpp
@@ -1,12 +1,32 @@
-#pragma  once
+#pragma once
+#include <functional>
+#include <map>
 #include <libplatform/libplatform.h>
 #include <v8.h>
-using namespace v8;
+#include "V8BindObject.hpp"
+#include "pro/types/generictypes.hpp"
+
 namespace xmax {
 	namespace scriptv8 {
-		class V8ui256
+		class V8u256 : public V8BindObject<V8u256>
 		{
-			int data[8];
+		public:
+			V8u256(pro::uint128 value) :value_(value) {}
+
+			V8u256() :value_(0) {}
+
+			operator pro::uint256() { return value_; }
+
+			static constexpr inline const char* TypeName() {
+				return "V8u256";
+			}
+
+		
+
+		private:
+			pro::uint256 value_;
 		};
+
+
 	}
 }
