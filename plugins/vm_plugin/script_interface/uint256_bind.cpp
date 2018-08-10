9696d7751a36c2a42db7005fe05328f95d0f8809
luow1226@sina.com <luow1226@sina.com>
2018-08-10 10:18:36 +0800

    U256 bind

--- plugins/vm_plugin/script_interface/uint256_bind.cpp
+++ plugins/vm_plugin/script_interface/uint256_bind.cpp
@@ -3,7 +3,7 @@ namespace xmax {
 
 	namespace scriptv8 {
 
-		xmax::scriptv8::V8u256::V8u256* V8u256::NewV8CppObj(const v8::FunctionCallbackInfo<v8::Value>& args)
+		V8u256* V8u256::NewV8CppObj(const v8::FunctionCallbackInfo<v8::Value>& args)
 		{
 
 		}
