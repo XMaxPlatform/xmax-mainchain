4d9fecb5fb2ce926a43517982c0ff255eea1953b
ray <254346460@qq.com>
2018-08-13 17:23:33 +0800

    Add log message class

---
+++ foundation/pro/include/pro/log/log_message.hpp
@@ -0,0 +1,21 @@
+/**
+*  @file
+*  @copyright defined in xmax/LICENSE
+*/
+
+#pragma once
+#include <string>
+
+namespace pro {
+
+	class LogMessage {
+	public:
+		LogMessage() {}
+		~LogMessage() {}
+
+	private:
+		std::string msg_content_;
+
+
+	};
+}
---
+++ foundation/pro/log/log_message.cpp
@@ -0,0 +1,6 @@
+#include "pro/log/log_message.hpp"
+
+
+namespace pro {
+
+}
