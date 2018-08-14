367d64034c06a380328472a54c760928d36ea89f
wenyang <419962937@qq.com>
2018-08-14 12:04:22 +0800

    fix bugs

--- foundation/pro/include/pro/crypto/Base58.hpp
+++ foundation/pro/include/pro/crypto/Base58.hpp
@@ -1,1 +1,84 @@
 #pragma once
+
+#include <string>
+#include <vector>
+
+namespace pro
+{
+	/**
+	* Encode a byte sequence as a base58-encoded string.
+	* pbegin and pend cannot be NULL, unless both are.
+	*/
+	std::string EncodeBase58(const unsigned char* pbegin, const unsigned char* pend);
+
+	/**
+	* Encode a byte vector as a base58-encoded string
+	*/
+	std::string EncodeBase58(const std::vector<unsigned char>& vch);
+
+	/**
+	* Decode a base58-encoded string (psz) into a byte vector (vchRet).
+	* return true if decoding is successful.
+	* psz cannot be NULL.
+	*/
+	bool DecodeBase58(const char* psz, std::vector<unsigned char>& vchRet);
+
+	/**
+	* Decode a base58-encoded string (str) into a byte vector (vchRet).
+	* return true if decoding is successful.
+	*/
+	bool DecodeBase58(const std::string& str, std::vector<unsigned char>& vchRet);
+
+	/**
+	* Encode a byte vector into a base58-encoded string, including checksum
+	*/
+	std::string EncodeBase58Check(const std::vector<unsigned char>& vchIn);
+
+	/**
+	* Decode a base58-encoded string (psz) that includes a checksum into a byte
+	* vector (vchRet), return true if decoding is successful
+	*/
+	inline bool DecodeBase58Check(const char* psz, std::vector<unsigned char>& vchRet);
+
+	/**
+	* Decode a base58-encoded string (str) that includes a checksum into a byte
+	* vector (vchRet), return true if decoding is successful
+	*/
+	inline bool DecodeBase58Check(const std::string& str, std::vector<unsigned char>& vchRet);
+
+	class Base58String;
+	class PrivateKey;
+
+	class Base58Data
+	{
+	public:
+
+		void SetData(const std::vector<unsigned char> &vchVersionIn, const void* pdata, size_t nSize);
+		void SetData(const std::vector<unsigned char> &vchVersionIn, const unsigned char *pbegin, const unsigned char *pend);
+
+		bool SetStringWithVersionBytes(const char* psz, unsigned int nVersionBytes);
+		bool SetString(const char* psz) {
+			return SetStringWithVersionBytes(psz, 1);
+		}
+		bool SetString(const std::string& str);
+		bool SetBase58string(const Base58String& str);
+
+		std::string ToString() const;
+		int CompareTo(const Base58Data& b58) const;
+
+		bool operator==(const Base58Data& b58) const { return CompareTo(b58) == 0; }
+		bool operator<=(const Base58Data& b58) const { return CompareTo(b58) <= 0; }
+		bool operator>=(const Base58Data& b58) const { return CompareTo(b58) >= 0; }
+		bool operator< (const Base58Data& b58) const { return CompareTo(b58) < 0; }
+		bool operator> (const Base58Data& b58) const { return CompareTo(b58) > 0; }
+		friend class PrivateKey;
+	protected:
+
+
+	private:
+
+		std::vector<unsigned char>  vchVersion_;
+
+		std::vector<unsigned char>	vchData_;
+	};
+}
--- foundation/pro/include/pro/utils/sha256.hpp
+++ foundation/pro/include/pro/utils/sha256.hpp
@@ -16,14 +16,21 @@ public:
 
 	template<typename T> void Hash(const T& src);
 
+	void Hash(const std::vector<unsigned char>& src);
+
+	void Hash(std::vector<unsigned char>::iterator start, std::vector<unsigned char>::iterator end);
+	void Hash(std::vector<unsigned char>::const_iterator cstart, std::vector<unsigned char>::const_iterator cend);
+
 	const std::string& GetHex() const;
 
-	const char*	Data() const;
+	const unsigned char* Data() const;
+
+	const std::vector<unsigned char>& GetDataVec() const;
 
 private:
 	
-	mutable std::string		hexHashStr_;
-	std::vector<char>		hash_;
+	mutable std::string				hexHashStr_;
+	std::vector<unsigned char>		hash_;
 };
 
 template<typename T>
@@ -36,11 +43,26 @@ void CSHA256::Hash(const T& src)
 
 	size_t nSize = sbuf.pubseekoff(0, std::ios_base::end);
 	sbuf.pubseekoff(0, std::ios_base::beg);
-	std::vector<char> vec(nSize);
+	std::vector<unsigned char> vec(nSize);
 	sbuf.sgetn(&vec[0], nSize);
 
 	picosha2::hash256(vec.begin(), vec.end(), hash_.begin(), hash_.end());
 
+}
+
+inline void CSHA256::Hash(const std::vector<unsigned char>& src)
+{
+	picosha2::hash256(src.begin(), src.end(), hash_);
+}
+
+inline void CSHA256::Hash(std::vector<unsigned char>::iterator start, std::vector<unsigned char>::iterator end)
+{
+	picosha2::hash256(start, end, hash_);
+}
+
+inline void CSHA256::Hash(std::vector<unsigned char>::const_iterator cstart, std::vector<unsigned char>::const_iterator cend)
+{
+	picosha2::hash256(cstart, cend, hash_);
 }
 
 inline const std::string& CSHA256::GetHex() const
@@ -53,9 +75,51 @@ inline const std::string& CSHA256::GetHex() const
 	return hexHashStr_;
 }
 
-inline const char* CSHA256::Data() const
+inline const unsigned char* CSHA256::Data() const
 {
 	return &hash_[0];
+}
+
+inline const std::vector<unsigned char>& CSHA256::GetDataVec() const
+{
+	return hash_;
+}
+
+class DoubleSHA256
+{
+public:
+
+	void Hash(const std::vector<unsigned char>& src);
+
+	void Hash(std::vector<unsigned char>::iterator start, std::vector<unsigned char>::iterator end);
+
+	const unsigned char* Data() const;
+
+protected:
+private:
+
+	CSHA256		sha256_;
+};
+
+inline void DoubleSHA256::Hash(const std::vector<unsigned char>& src)
+{
+	CSHA256 firstHash;
+	firstHash.Hash(src);
+	const std::vector<unsigned char>& hash = firstHash.GetDataVec();
+	sha256_.Hash(hash);
+}
+
+inline void DoubleSHA256::Hash(std::vector<unsigned char>::iterator start, std::vector<unsigned char>::iterator end)
+{
+	CSHA256 firstHash;
+	firstHash.Hash(start, end);
+	const std::vector<unsigned char>& hash = firstHash.GetDataVec();
+	sha256_.Hash(hash.begin(), hash.end() );
+}
+
+inline const unsigned char* DoubleSHA256::Data() const
+{
+	return sha256_.Data();
 }
 
 }
