2d6e8270ea52b2f917ff256ce4ffadcb02c21c79
karlvin <kars.lib@gmail.com>
2018-08-15 10:01:57 +0800

    describe

--- plugins/contractutil_plugin/include/contractutil_plugin.hpp
+++ plugins/contractutil_plugin/include/contractutil_plugin.hpp
@@ -23,7 +23,7 @@ namespace xmax
 		virtual void Initialize(const xmaxapp::VarsMap& options) override;
 		//start up 3rd libarary
 		virtual void Startup() override;
-		//
+		// shut down
 		virtual void Shutdown() override;
 
 
