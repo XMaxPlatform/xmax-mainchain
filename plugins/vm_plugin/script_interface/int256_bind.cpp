2162d43984146222651dc30686532a1e658ad769
luow1226@sina.com <luow1226@sina.com>
2018-08-16 19:03:25 +0800

    add i256

--- plugins/vm_plugin/script_interface/int256_bind.cpp
+++ plugins/vm_plugin/script_interface/int256_bind.cpp
@@ -1,6 +1,18 @@
-#include "int256_bind.hpp"
-//imp 256
+#pragma once
+#include <functional>
+#include <map>
+#include <libplatform/libplatform.h>
+#include <v8.h>
+#include "V8BindObject.hpp"
+#include "pro/types/generictypes.hpp"
+
 namespace xmax {
 	namespace scriptv8 {
+		class V8i256 : public V8BindObject<V8i256>
+		{
+		
+		};
+
+
 	}
 }
