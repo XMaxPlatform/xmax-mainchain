a89a78993c63f15ce6245ceb5614f759ba33626c
SAIKYOU1986 <419962937@qq.com>
2018-08-17 22:46:15 +0800

    add comment

--- foundation/pro/include/pro/utils/sha256.hpp
+++ foundation/pro/include/pro/utils/sha256.hpp
@@ -14,7 +14,7 @@ public:
 	CSHA256();
 
 	~CSHA256();
-
+	//calculate hash value for a non pod type
 	template<typename T> void Hash(const T& src);
 
 	void Hash(const std::vector<unsigned char>& src);
