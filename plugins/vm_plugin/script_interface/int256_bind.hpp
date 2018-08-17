2d2c7f4049b0a3067da55cf5275b803598daf7e5
luow1226@sina.com <luow1226@sina.com>
2018-08-17 16:53:45 +0800

    add bind

--- plugins/vm_plugin/script_interface/int256_bind.hpp
+++ plugins/vm_plugin/script_interface/int256_bind.hpp
@@ -11,7 +11,7 @@ namespace xmax {
 		class V8i256 : public V8BindObject<V8i256>
 		{
 		public:
-			V8i256() {}
+			V8i256():value_(){}
 			~V8i256() {}
 			static V8i256* NewV8CppObj(const v8::FunctionCallbackInfo<v8::Value>& args);
 			static void RegisterWithV8(v8::Isolate* isolate, v8::Handle<v8::ObjectTemplate> global);
