0a3da6b990ea222360438aee4b372837248508f0
fengzi <liuxingzdh@163.com>
2018-08-20 10:44:40 +0800

    dbindexed.hpp

---
+++ foundation/unitedb/include/unitedb/dbindexed.hpp
@@ -0,0 +1,15 @@
+/**
+*  @file
+*  @copyright defined in xmax/LICENSE
+*/
+#pragma once
+
+#include <boost/multi_index_container.hpp> 
+#include <boost/multi_index/ordered_index.hpp> 
+#include <boost/multi_index/member.hpp>
+
+namespace unitedb
+{
+
+
+}
