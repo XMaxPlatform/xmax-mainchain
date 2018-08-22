f26b726155f813572467c0854da33f7d3115b1d1
fengzi <liuxingzdh@163.com>
2018-08-22 11:57:07 +0800

    chain db base.

---
+++ blockchain/chain/include/objects/account.hpp
@@ -0,0 +1,24 @@
+/**
+*  @file
+*  @copyright defined in xmax/LICENSE
+*/
+#pragma once
+#include <objects/objectbase.hpp>
+
+namespace chain
+{
+	DBOBJ_CLASS(Account, ObjectType::AccountObject)
+	{
+		DBOBJ_BODY(Account,
+			(DB_SFIELD(AccountName, name))
+		)
+	};
+
+	struct ByName;
+	using AccountIdx = db::DBIndexedBy<Account,
+		indexed::ordered_unique<indexed::tag<ByName>, indexed::member<Account, AccountName, &Account::name>>
+
+	>;
+
+	using AccountTable = db::DBTable<AccountIdx>;
+}
