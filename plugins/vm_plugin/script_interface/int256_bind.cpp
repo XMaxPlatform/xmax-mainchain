cae69a821dd1d5761866ea7d71649272165d9772
luow1226@sina.com <luow1226@sina.com>
2018-08-22 19:35:35 +0800

    i256 bind

--- plugins/vm_plugin/script_interface/int256_bind.cpp
+++ plugins/vm_plugin/script_interface/int256_bind.cpp
@@ -27,7 +27,9 @@ namespace xmax {
 		void V8i256::WeakExternalReferenceCallback(const v8::WeakCallbackInfo<V8i256>& data)
 		{
 			if (V8i256* cpp_object = data.GetParameter())
-		
+			{
+				delete cpp_object;
+			}
 		}
 
 	}
