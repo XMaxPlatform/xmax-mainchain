d991db48707f8a173bc219145e465295eb4a4922
wenyang <419962937@qq.com>
2018-09-03 18:27:29 +0800

    add bitutil

---
+++ blockchain/chain/chain_utils.cpp
@@ -0,0 +1,12 @@
+#include "chain_utils.hpp"
+#include "pro/utils/bitutil.hpp"
+
+namespace chain
+{
+	uint32_t num_from_id(const CSHA256& id)
+	{
+		uint64_t ut;
+		memcpy(&ut, id.Data(), 64);
+		return pro::endian_reverse_u32(ut);
+	}
+}
--- blockchain/chain/include/chain_utils.hpp
+++ blockchain/chain/include/chain_utils.hpp
@@ -1,1 +1,9 @@
 #pragma once
+
+#include "builder_rule.hpp"
+#include "pro/types/generictypes.hpp"
+
+namespace chain
+{
+	uint32_t num_from_id(const CSHA256& id);
+}
