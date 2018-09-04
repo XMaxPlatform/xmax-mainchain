2eb310e0b7c9c3285215c16ad1c891dd91f7400f
wenyang <419962937@qq.com>
2018-09-04 20:13:58 +0800

    add function

--- blockchain/chain/chain_utils.cpp
+++ blockchain/chain/chain_utils.cpp
@@ -21,4 +21,27 @@ namespace chain
 		memcpy(&ret.GetDataVec()[0], &ut, 64);
 		return ret;
 	}
+
+	CSHA256 cal_merkle(std::vector<CSHA256> digests)
+	{
+		if (digests.size() == 0)
+		{
+			return CSHA256();
+		}
+		while (digests.size() >1)
+		{
+			if (digests.size() % 2)
+			{
+				digests.push_back(digests.back());
+			}
+			for (int i = 0; i < digests.size() / 2; ++i)
+			{
+				CSHA256 sha;
+				sha.Hash(std::make_pair(digests[i], digests[i + 1]));
+				digests[i / 2] = sha;
+			}
+			digests.resize(digests.size() / 2);
+		}
+		return digests.front();
+	}
 }
--- blockchain/chain/include/chain_utils.hpp
+++ blockchain/chain/include/chain_utils.hpp
@@ -8,4 +8,6 @@ namespace chain
 	uint32_t num_from_id(const BlockID& id);
 
 	CSHA256 block_id(const CSHA256& summary, uint32_t num);
+
+	CSHA256 cal_merkle(std::vector<CSHA256> digests);
 }
