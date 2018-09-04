63be5557ef7f8afbd5fe2c94dfcbc3e0150e5686
ray <254346460@qq.com>
2018-09-04 10:52:22 +0800

    Remove old line

--- foundation/pro/include/pro/exception/exception_base.hpp
+++ foundation/pro/include/pro/exception/exception_base.hpp
@@ -95,7 +95,6 @@ namespace pro
 		Exception(int _type, const string& _description, const string& _source, const char* _tile, const char* _file, long _line);
 		Exception(int _type, const string& _description, const char* _tile, const char* _file, long _line);
 
-		long line_;
 		int type_;
 		string title_;
 		string description_;
