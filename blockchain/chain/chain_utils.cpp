638b36eabf53e1e8aa9bbd1c1fdf378d4d9620c7
fengzi <liuxingzdh@163.com>
2018-09-05 10:41:59 +0800

    rename some type.

--- blockchain/chain/chain_utils.cpp
+++ blockchain/chain/chain_utils.cpp
@@ -4,44 +4,47 @@
 
 namespace chain
 {
-	uint32_t num_from_id(const BlockID& id)
+	namespace utils
 	{
-		uint64_t ut;
-		memcpy(&ut, id.Data(), 64);
-		return pro::endian_reverse_u32(ut);
-	}
-
-	CSHA256 block_id(const CSHA256& summary, uint32_t num)
-	{
-		uint64_t ut;
-		memcpy(&ut, summary.Data(), 64);
-		ut &= 0xffffffff00000000;
-		ut += pro::endian_reverse_u32(num);
-		CSHA256 ret(summary.GetDataVec());
-		memcpy(&ret.GetDataVec()[0], &ut, 64);
-		return ret;
-	}
-
-	CSHA256 cal_merkle(std::vector<CSHA256> digests)
-	{
-		if (digests.size() == 0)
+		uint32_t num_from_id(const BlockID& id)
 		{
-			return CSHA256();
+			uint64_t ut;
+			memcpy(&ut, id.Data(), 64);
+			return pro::endian_reverse_u32(ut);
 		}
-		while (digests.size() >1)
+
+		BlockID block_id(const BlockID& summary, uint32_t num)
 		{
-			if (digests.size() % 2)
-			{
-				digests.push_back(digests.back());
-			}
-			for (int i = 0; i < digests.size() / 2; ++i)
-			{
-				CSHA256 sha;
-				sha.Hash(std::make_pair(digests[i], digests[i + 1]));
-				digests[i / 2] = sha;
-			}
-			digests.resize(digests.size() / 2);
+			uint64_t ut;
+			memcpy(&ut, summary.Data(), 64);
+			ut &= 0xffffffff00000000;
+			ut += pro::endian_reverse_u32(num);
+			BlockID ret(summary.GetDataVec());
+			memcpy(&ret.GetDataVec()[0], &ut, 64);
+			return ret;
 		}
-		return digests.front();
+
+		HashDigest cal_merkle(std::vector<HashDigest> digests)
+		{
+			if (digests.size() == 0)
+			{
+				return HashDigest();
+			}
+			while (digests.size() > 1)
+			{
+				if (digests.size() % 2)
+				{
+					digests.push_back(digests.back());
+				}
+				for (int i = 0; i < digests.size() / 2; ++i)
+				{
+					HashDigest sha;
+					sha.Hash(std::make_pair(digests[i], digests[i + 1]));
+					digests[i / 2] = sha;
+				}
+				digests.resize(digests.size() / 2);
+			}
+			return digests.front();
+		}
 	}
 }
--- blockchain/chain/include/chain_utils.hpp
+++ blockchain/chain/include/chain_utils.hpp
@@ -1,13 +1,15 @@
 #pragma once
 
-#include "builder_rule.hpp"
-#include "pro/types/generictypes.hpp"
+#include <chain_types.hpp>
 
 namespace chain
 {
-	uint32_t num_from_id(const BlockID& id);
+	namespace utils
+	{
+		uint32_t num_from_id(const BlockID& id);
 
-	CSHA256 block_id(const CSHA256& summary, uint32_t num);
+		BlockID block_id(const BlockID& summary, uint32_t num);
 
-	CSHA256 cal_merkle(std::vector<CSHA256> digests);
+		HashDigest cal_merkle(std::vector<HashDigest> digests);
+	}
 }
