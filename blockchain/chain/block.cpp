482b7f6d6ebf58141afcf12fc608f7ecb2ded446
fengzi <liuxingzdh@163.com>
2018-09-06 10:07:32 +0800

    rename block class.

--- blockchain/chain/block.cpp
+++ blockchain/chain/block.cpp
@@ -3,19 +3,19 @@
 
 namespace chain
 {
-	HashDigest block_header::Digest() const
+	HashDigest BlockHeader::Digest() const
 	{
 		HashDigest sha;
 		sha.Hash(*this);
 		return sha;
 	}
 
-	uint32_t block_header::Block_Num() const
+	uint32_t BlockHeader::Block_Num() const
 	{
 		return utils::num_from_id(previous_) + 1;
 	}
 
-	BlockID signed_block_header::Id() const
+	BlockID SignedBlockHeader::Id() const
 	{
 		return utils::block_id(Digest(), Block_Num());
 	}
--- blockchain/chain/include/block.hpp
+++ blockchain/chain/include/block.hpp
@@ -8,7 +8,7 @@
 
 namespace chain
 {
-	class block_header
+	class BlockHeader
 	{
 		RF_BODY(
 			(RF_SFIELD(BlockID, previous_))
@@ -20,23 +20,19 @@ namespace chain
 		uint32_t   Block_Num() const;
 	};
 
-	class signed_block_header : public block_header
+	class SignedBlockHeader : public BlockHeader
 	{
-		RF_BODY_INHERIT(block_header,
+		RF_BODY_INHERIT(BlockHeader,
 			(RF_SFIELD(Signature, builder_signature_))
 		)
 	public:
 		
 		BlockID				Id() const;
-		PublicKey			Get_Signer_Key() const;
-		void				Sign(const PrivateKey& signer);
-		bool                Is_Signer_Valid(const PublicKey& signer_key) const;
 	};
 
-	class transaction_receipt_header
+	class TransactionReceipt
 	{
-	public:
-
+	public:	
 		enum result_code : uint8_t
 		{
 			applied = 0,
@@ -46,40 +42,12 @@ namespace chain
 		uint64_t								receipt_idx;
 	};
 
-	class transaction_receipt : public transaction_receipt_header
+	class SignedBlock : public SignedBlockHeader
 	{
 	public:
-
-		HashDigest		Cal_Digest() const;
+		std::vector<TransactionReceipt> receipts_;
 	};
 
-	class signed_block : public signed_block_header
-	{
-	public:
-		std::vector<transaction_receipt> receipts_;
-	};
 
-	class signed_block_list
-	{
-	public:
-		std::vector<signed_block>	blockList_;
-	};
-
-	using signed_block_ptr = std::shared_ptr<signed_block>;
-
-	class block_confirmation_header
-	{
-	public:
-
-		BlockID			block_id_;
-		Name			verifier_;
-
-	};
-
-	class block_confirmation : public block_confirmation_header
-	{
-	public:
-
-		Signature		builder_signature_;
-	};
+	using SignedBlockPtr = std::shared_ptr<SignedBlock>;
 }
