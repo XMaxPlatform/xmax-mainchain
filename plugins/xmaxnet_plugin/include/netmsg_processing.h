7b571ee164ef5f884d72f59e44d101894855cb7e
wenyang <419962937@qq.com>
2018-09-06 19:41:31 +0800

    add comment

--- plugins/xmaxnet_plugin/include/netmsg_processing.h
+++ plugins/xmaxnet_plugin/include/netmsg_processing.h
@@ -9,7 +9,7 @@ namespace xmax
 	class XMX_Connection;
 	class XmaxNetPluginImpl;
 	using namespace google::protobuf;
-
+	// process all  net messages
 	class NetMsgProcessing : public pro::Singleton<NetMsgProcessing>
 {
 	public:
@@ -17,9 +17,11 @@ namespace xmax
 		void	ParseMsg(const char* pMsg, const MsgHeader& header, std::shared_ptr<XMX_Connection> pConnect);
 		void    SetNetImpl(const std::shared_ptr<XmaxNetPluginImpl>& impl);
 	protected:
-
+		// test msg
 		void _OnHandleMsg(std::shared_ptr<XMX_Connection> pConnect, const HelloMsg& msg);
+		// version msg
 		void _OnHandleMsg(std::shared_ptr<XMX_Connection> pConnect, const VersionMsg& msg);
+		// version ack msg
 		void _OnHandleMsg(std::shared_ptr<XMX_Connection> pConnect, const VerAckMsg& msg);
 		void _OnHandleMsg(std::shared_ptr<XMX_Connection> pConnect, const AddrMsg& msg);
 		void _OnHandleMsg(std::shared_ptr<XMX_Connection> pConnect, const GetAddrMsg& msg);
