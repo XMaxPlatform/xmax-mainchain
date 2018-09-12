3a5bb5b58a2f4d967b7a137634b86255989660b4
fengzi <liuxingzdh@163.com>
2018-09-12 10:28:36 +0800

    pending block.

---
+++ blockchain/chain/include/pending_block.hpp
@@ -0,0 +1,18 @@
+/**
+*  @file
+*  @copyright defined in xmax/LICENSE
+*/
+#pragma once
+
+#include <chaintypes.hpp>
+#include <chain.hpp>
+#include <chain_db.hpp>
+#include <unitedb/database.hpp>
+
+namespace chain
+{
+	struct PendingBlock
+	{
+
+	};
+}
